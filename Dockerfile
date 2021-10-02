c801aca689ac007417b2d019e6fe9f6f251f8647
jerrywang <jeerywa@gmail.com>
2021-10-02 16:51:53 +0800

    remove env variables in backend container and add viper to backend container

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,9 @@ FROM alpine:3.14
 EXPOSE 8000
 WORKDIR /app
 COPY --from=builder /app/main /app
+COPY config/ /app/config/
+RUN addgroup -S appgroup && \
+    adduser -S appuser -G appgroup && \
+    chown appuser:appgroup -R /app
+USER appuser
 ENTRYPOINT /app/main
--- main.go
+++ main.go
@@ -4,39 +4,25 @@ import (
     "fmt"
     "net/http"
     "log"
-    "database/sql"
-    _ "github.com/lib/pq"
+    "github.com/spf13/viper"
 )
 
-const (
-    host     = "db"
-    port     = 5432
-    user     = "jerry"
-    password = "jerry0315"
-    dbname   = "queue_system"
-  )
+func init() {
+	viper.SetConfigFile(`./config/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello world~~")
+    env := viper.GetString(`env`)
+    fmt.Fprintf(w, env)
 }
 
 func main() {
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-        "password=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        panic(err)
-    }
-    defer db.Close()
-
-    err = db.Ping()
-    if err != nil {
-        panic(err)
-    }
-
     http.HandleFunc("/", hello)
-    err = http.ListenAndServe("0.0.0.0:8000", nil)
+    err := http.ListenAndServe("0.0.0.0:8000", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
