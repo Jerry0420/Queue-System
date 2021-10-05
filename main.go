54e29099671df6f7206da9cb40975c37274a0558
jerrywang <jeerywa@gmail.com>
2021-10-06 00:13:55 +0800

    1. add response wrapper. 2.add response format

--- main.go
+++ main.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"log"
 	"net/http"
-
+    "io"
+	"encoding/json"
 	"github.com/jerry0420/queue-system/config"
 	"github.com/jerry0420/queue-system/logging"
 	"github.com/jerry0420/queue-system/utils"
 )
 
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        logger := logging.NewLogger([]string{"requestID", "duration", "code"}, false)        
+        ctx := context.WithValue(r.Context(), "requestID", "aaaaaaaaaa")
+        r = r.WithContext(ctx)
+        
+        responseWrapper := &utils.ResponseWrapper{
+            ResponseWriter: w,
+            Buffer: &bytes.Buffer{},
+        }
+
+        next(responseWrapper, r)
+        
+        var wrapperResponse *utils.ResponseFormat
+        json.Unmarshal(responseWrapper.Buffer.Bytes(), &wrapperResponse)
+        ctx = context.WithValue(r.Context(), "code", wrapperResponse.Code)
+        ctx = context.WithValue(ctx, "duration", 3)
+        
+        io.Copy(w, responseWrapper.Buffer)
+        r = r.WithContext(ctx)
+        
+        logger.INFOf(r.Context(), "hello world %d", 1234)
+    }
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-    // logger := logging.NewLogger([]string{"requestID", "duration"}, false)
-    ctx := context.WithValue(r.Context(), "requestID", "aaaaaaaaaa")
-    ctx = context.WithValue(ctx, "duration", 3)
-    // logger.INFOf(ctx, "hello world %d", 1234)
 
     serverConfig := config.NewConfig()
     
-    utils.JsonResponse(w, ctx, map[string]interface{}{
+    utils.JsonResponse(w, map[string]interface{}{
         "page": serverConfig.CONTEXT_TIMEOUT(),
         "list": []string{"hello", "world"},
     }, utils.ServerError40001)
-
-    logger := logging.NewLogger([]string{"requestID", "duration", "code"}, false)
-    logger.INFOf(ctx, "hello world %d", 1234)
 }
 
 func main() {
-    http.HandleFunc("/", hello)
+    http.HandleFunc("/", middleware(hello))
     err := http.ListenAndServe("0.0.0.0:8000", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
--- utils/response.go
+++ utils/response.go
@@ -1,16 +1,30 @@
 package utils
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
+	"bytes"
 )
 
-func JsonResponseOK(w http.ResponseWriter, ctx context.Context, response interface{}) {
-	JsonResponse(w, ctx, response, nil)
+type ResponseWrapper struct {
+    http.ResponseWriter
+    Buffer *bytes.Buffer
 }
 
-func JsonResponse(w http.ResponseWriter, ctx context.Context, response interface{}, serverError *ServerError) {
+func (responseWrapper *ResponseWrapper) Write(p []byte) (int, error) {
+    return responseWrapper.Buffer.Write(p)
+}
+
+type ResponseFormat struct {
+	Code interface{} `json:"code"`
+	Data interface{} `json:"data"`
+}
+
+func JsonResponseOK(w http.ResponseWriter, response interface{}) {
+	JsonResponse(w, response, nil)
+}
+
+func JsonResponse(w http.ResponseWriter, response interface{}, serverError *ServerError) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var code int
@@ -21,17 +35,14 @@ func JsonResponse(w http.ResponseWriter, ctx context.Context, response interface
 	} else {
 		code = 20001
 	}
-	
-	ctx = context.WithValue(ctx, "code", code)
-
 
 	statusCode = code / 100 //http sattus code is the first two digits of code.
 	w.WriteHeader(statusCode)
 
 	json.NewEncoder(w).Encode(
-		&map[string]interface{}{
-			"data": response,
-			"code": code,
+		&ResponseFormat{
+			Code: code,
+			Data: response,
 		},
 	)
 }
