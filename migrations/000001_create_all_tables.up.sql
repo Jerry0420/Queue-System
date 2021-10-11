e80317963d90b12c13e288956267cfda7913fc3a
jerrywang <jeerywa@gmail.com>
2021-10-11 20:51:13 +0800

    add migrations of up file.

--- migrations/000001_create_all_tables.up.sql
+++ migrations/000001_create_all_tables.up.sql
@@ -1,4 +1,14 @@
-CREATE TABLE IF NOT EXISTS users(
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE store_status AS ENUM ('normal', 'close');
+
+CREATE TABLE IF NOT EXISTS store(
    id serial PRIMARY KEY,
-   username VARCHAR (50) UNIQUE NOT NULL
+   email varchar(512) NOT NULL,
+   password varchar(15) NOT NULL,
+   name varchar(64) NOT NULL,
+   description text,
+   created_at timestamp NOT NULL DEFAULT clock_timestamp(),
+   status store_status NOT NULL,
+   updated_at timestamp NOT NULL DEFAULT clock_timestamp(),
 );
