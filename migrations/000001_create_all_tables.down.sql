fcd09c5bc76d5a41011e3c0287ffbd309ebfdc70
jerrywang <jeerywa@gmail.com>
2021-10-12 14:36:20 +0800

    add all tables in the migration file

--- migrations/000001_create_all_tables.down.sql
+++ migrations/000001_create_all_tables.down.sql
@@ -1,1 +1,11 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS customers;
+
+DROP TABLE IF EXISTS queues;
+
+DROP TABLE IF EXISTS stores;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
+
+DROP TYPE IF EXISTS store_status;
+
+DROP TYPE IF EXISTS customer_status;
--- migrations/000001_create_all_tables.up.sql
+++ migrations/000001_create_all_tables.up.sql
@@ -1,8 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TYPE store_status AS ENUM ('normal', 'close');
+-- ----------------------------
 
-CREATE TABLE IF NOT EXISTS store(
+CREATE TYPE store_status AS ENUM ('open', 'close');
+
+CREATE TABLE IF NOT EXISTS stores(
    id serial PRIMARY KEY,
    email varchar(512) NOT NULL,
    password varchar(15) NOT NULL,
@@ -10,5 +12,28 @@ CREATE TABLE IF NOT EXISTS store(
    description text,
    created_at timestamp NOT NULL DEFAULT clock_timestamp(),
    status store_status NOT NULL,
-   updated_at timestamp NOT NULL DEFAULT clock_timestamp(),
+   session_id uuid NOT NULL DEFAULT uuid_generate_v4()
 );
+
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS queues(
+   id serial PRIMARY KEY,
+   name varchar(64) NOT NULL,
+   store_id integer REFERENCES stores ON DELETE CASCADE
+);
+
+-- ----------------------------
+
+CREATE TYPE customer_status AS ENUM ('processing', 'done', 'delete');
+
+CREATE TABLE IF NOT EXISTS customers(
+   id serial PRIMARY KEY,
+   name varchar(64) NOT NULL,
+   phone varchar(30) NOT NULL,
+   queue_id integer REFERENCES queues ON DELETE CASCADE,
+   status customer_status NOT NULL,
+   created_at timestamp NOT NULL DEFAULT clock_timestamp()
+);
+
+-- ----------------------------
