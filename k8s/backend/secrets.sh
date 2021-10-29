7647dc76b7cab2c06cff01946c00b5bafe7a112a
jerrywang <jeerywa@gmail.com>
2021-10-30 00:27:23 +0800

    update secrets.sh

--- k8s/backend/secrets.sh
+++ k8s/backend/secrets.sh
@@ -7,8 +7,8 @@ metadata:
   namespace: queue-system
   name: backend-secret
 data:
-  VAULT_CRED_NAME: $(echo -n "vault_db" | base64 -w0)
-  VAULT_ROLE_ID: $(echo -n "550693ef-d956-a588-6af7-720583c20a5d" | base64 -w0)
+  VAULT_CRED_NAME: $(echo -n "vault_db" | base64)
+  VAULT_ROLE_ID: $(echo -n "550693ef-d956-a588-6af7-720583c20a5d" | base64)
 EOF
 
 # ./secrets.sh | kubectl apply -f -
