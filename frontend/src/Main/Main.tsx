7cb08493beaf70559151798e49d5e61cf7aa9f52
jerrywang <jeerywa@gmail.com>
2022-02-17 17:05:31 +0800

    remove redux

--- frontend/src/Main/Main.tsx
+++ frontend/src/Main/Main.tsx
@@ -3,7 +3,7 @@ import '../styles/Main.scss'
 import classNames from "classnames"
 
 const Main = () => {
-  const divClass = classNames("main", "text-3xl", "font-bold", "underline")
+  const divClass = classNames("main", "font-bold", "underline")
   return (
     <div className={divClass}>
       hello world
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Main from './Main'
+import Main from './Main/Main'
 import './tailwind.scss'
 
 ReactDom.render(<Main />, document.getElementById('root'))
