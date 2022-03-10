ee38976ab59dbb4cecaf58acb2c3f43d12871bc4
jerrywang <jeerywa@gmail.com>
2022-03-10 16:04:18 +0800

    refactor checkauthflow

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react"
-import { ACTION_TYPES } from "../apis/reducer"
 import { useNavigate } from "react-router-dom"
 import { checkExistenceOfRefreshableCookie } from "../apis/helper"
 
@@ -11,9 +10,9 @@ const SignUp = () => {
     if (checkExistenceOfRefreshableCookie() === true) {
       const storeId = localStorage.getItem("storeId")
       if (storeId != null) {
-        // navigate("/temp")
         navigate(`/stores/${storeId}`)
       } else {
+        // remove refreshable cookie for signup again
         document.cookie = "refreshable=true ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
       }
     }
--- frontend/src/components/Store.tsx
+++ frontend/src/components/Store.tsx
@@ -3,31 +3,34 @@ import { useParams, Link, useNavigate } from "react-router-dom"
 import { RefreshTokenContext } from "./contexts"
 import { createSessionWithSSE } from "../apis/SessionAPIs"
 import { checkAuthFlow, validateResponseSuccess } from "../apis/helper"
-import { JSONResponse } from "../apis/reducer"
+import { ACTION_TYPES, JSONResponse, useApiRequest } from "../apis/reducer"
+import { toDataURL } from "qrcode"
+import { updateStoreDescription } from "../apis/StoreAPIs"
 
 const Store = () => {
-  let { storeId } = useParams()
+  let { storeId }: {storeId: string} = useParams()
   let navigate = useNavigate()
+  const [sessionScannedURL, setSessionScannedURL] = useState("")
+  const [qrcodeImageURL, setQrcodeImageURL] = useState("")
+  const [storeDescription, setStoreDescription] = useState("")
+  const [normalToken, setNormalToken] = useState("")
+
   const {refreshTokenAction, makeRefreshTokenRequest} = useContext(RefreshTokenContext)
-  const [sessionScannedURL, setSessionScannedURL] = useState(null)
+  const [updateStoreDescriptionAction, makeUpdateStoreDescriptionRequest] = useApiRequest(
+    ...updateStoreDescription(parseInt(storeId), normalToken, storeDescription)
+    )
 
-  useEffect(() => {
+  const handleInputStoreDescription = (e: React.ChangeEvent<HTMLElement>) => {
+    const { value }: { value: string } = e.target
+    setStoreDescription(value)
+  }
 
-    let createSessionSSE: EventSource
+  const doCheckAuthFlow = (nextStuff: () => void) => {
     checkAuthFlow(refreshTokenAction.response, makeRefreshTokenRequest, 
       // nextStuff
       () => {
         if (validateResponseSuccess(refreshTokenAction.response) === true) {
-          const sessionToken: string = ((refreshTokenAction.response as JSONResponse)["session_token"] as string)
-          createSessionSSE = createSessionWithSSE(sessionToken)
-
-          createSessionSSE.onmessage = (event) => {
-            setSessionScannedURL(JSON.parse(event.data)["scanned_url"])
-          }
-          
-          createSessionSSE.onerror = (event) => {
-            createSessionSSE.close()
-          }
+          nextStuff()
         }
       }, 
       // redirectToMainPage
@@ -36,7 +39,42 @@ const Store = () => {
         navigate("/")
       }
     )
+  }
 
+  const doMakeUpdateStoreDescriptionRequest = () => {
+    doCheckAuthFlow(() => {
+      makeUpdateStoreDescriptionRequest()
+    })
+  }
+
+  useEffect(() => {
+    toDataURL(sessionScannedURL, (error, url) => {
+      if (url != null) {
+        setQrcodeImageURL(url)
+      }
+    })
+  }, [sessionScannedURL])
+
+  useEffect(() => {
+    if (validateResponseSuccess(refreshTokenAction.response) === true) {
+      setNormalToken(refreshTokenAction.response["token"])
+    }
+  }, [refreshTokenAction.response])
+
+  useEffect(() => {
+    let createSessionSSE: EventSource
+    doCheckAuthFlow(() => {
+      const sessionToken: string = ((refreshTokenAction.response as JSONResponse)["session_token"] as string)
+      createSessionSSE = createSessionWithSSE(sessionToken)
+
+      createSessionSSE.onmessage = (event) => {
+        setSessionScannedURL(JSON.parse(event.data)["scanned_url"])
+      }
+      
+      createSessionSSE.onerror = (event) => {
+        createSessionSSE.close()
+      }
+    })
     return () => {
       if (createSessionSSE != null) {
         createSessionSSE.close()
@@ -47,8 +85,17 @@ const Store = () => {
   return (
     <div>
         <Link to="/temp">to temp</Link>
-        {console.log(sessionScannedURL)}
-        {/* {console.log(refreshTokenAction)} */}
+        {/* <img src={qrcodeImageURL} alt="qrcode image"></img> */}
+
+        <br />
+        <input
+          type="text"
+          onChange={handleInputStoreDescription}
+          placeholder="input store description"
+        />
+        <button onClick={doMakeUpdateStoreDescriptionRequest}>
+          update store description
+        </button>
     </div>
   )
 }
